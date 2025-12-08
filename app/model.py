15f463901cf5398a3d69e95abda5e298cbc32177
akilm1998 <akilesh.m1998@gmail.com>
2025-12-08 19:22:50 +0530

    added app/model.py

--- app/model.py
+++ app/model.py
@@ -1,2 +1,9 @@
 from transformers import pipeline
-from functools import lru_cache
+from functools import lru_cache #least_recently_used
+
+@lru_cache()
+def get_pipeline():
+    return pipeline("text-classification",
+                    model="bvanaken/clinical-assertion-negation-bert",
+                    truncation=True
+                    )
